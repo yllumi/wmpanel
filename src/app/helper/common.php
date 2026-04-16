2a80f9eda2327a071c2a1bd903508ad98653ba18
Toni Haryanto <toha.samba@gmail.com>
2026-04-16 17:57:40 +0700

    Change function to site_url

--- src/app/helper/common.php
+++ src/app/helper/common.php
@@ -15,8 +15,8 @@ function sidebarMenus()
     return \Symfony\Component\Yaml\Yaml::parseFile($path) ?? [];
 }
 
-if (!function_exists('base_url')) {
-    function base_url(string $path = ''): string
+if (!function_exists('site_url')) {
+    function site_url(string $path = ''): string
     {
         $prefix = env('APP_BASE_PATH', ''); // isi '/p' di .env
         return $prefix . '/' . ltrim($path, '/');
